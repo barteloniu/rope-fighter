b1b4c950e1a825ebb4eda5d99f00eb74c6dd9712
barteloniu <barteloniu@gmail.com>
2016-12-09 23:53:13 +0100

    Added basic player script

---
+++ Assets/Skrypty/gracz.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gracz : MonoBehaviour
+{
+
+    public GameObject sprite;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            sprite.SetActive(true);
+            Vector3 myszka = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float angle = Mathf.Atan2(myszka.y - transform.position.y, myszka.x - transform.position.x) * (180 / Mathf.PI);
+
+            sprite.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+        else
+        {
+            sprite.SetActive(false);
+        }
+	}
+}
